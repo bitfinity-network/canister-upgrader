144c0ea9411510425a54e235579d091f84037643
Francesco <ufoscout@gmail.com>
2024-08-20 15:44:41 +0200

    refinements

--- src/upgrader_canister/tests/canister/mod.rs
+++ src/upgrader_canister/tests/canister/mod.rs
@@ -1,19 +1,17 @@
-use std::sync::Arc;
+use candid::Principal;
 
-use ic_canister_client::PocketIcClient;
-use upgrader_canister_client::UpgraderCanisterClient;
-
-use crate::pocket_ic::deploy_canister;
+use crate::pocket_ic::{build_client, deploy_canister};
 
 #[tokio::test]
 async fn test_should_query_build_data() {
-    let env = ic_exports::pocket_ic::init_pocket_ic().await;
-    let canister_principal = deploy_canister(&env).await;
-    let caller_principal = canister_principal;
-    let client = PocketIcClient::from_client(Arc::new(env), canister_principal, caller_principal);
-    let client = UpgraderCanisterClient::new(client);
+    // Arrange
+    let (pocket, canister_principal) = deploy_canister(None).await;
+    let caller_principal = Principal::anonymous();
+    let client = build_client(pocket, canister_principal, caller_principal);
 
+    // Act
     let result = client.get_canister_build_data().await.unwrap();
 
+    // Assert
     assert_eq!("upgrader_canister", result.pkg_name);
 }
--- src/upgrader_canister/tests/pocket_ic/mod.rs
+++ src/upgrader_canister/tests/pocket_ic/mod.rs
@@ -1,12 +1,19 @@
 use candid::{Encode, Principal};
+use ic_canister_client::PocketIcClient;
 use ic_exports::pocket_ic::PocketIc;
+use upgrader_canister_client::UpgraderCanisterClient;
 use upgrader_canister_did::UpgraderCanisterInitData;
 use wasm_utils::get_upgrader_canister_bytecode;
 
 pub mod wasm_utils;
 
 /// Deploys the upgrader canister and returns its principal
-pub async fn deploy_canister(env: &PocketIc) -> Principal {
+pub async fn deploy_canister(env: Option<PocketIc>) -> (PocketIc, Principal) {
+    let env = if let Some(env) = env {
+        env
+    } else {
+        ic_exports::pocket_ic::init_pocket_ic().await
+    };
     let wasm = get_upgrader_canister_bytecode();
     let init_data = UpgraderCanisterInitData {};
     let args = Encode!(&(init_data,)).unwrap();
@@ -14,5 +21,11 @@ pub async fn deploy_canister(env: &PocketIc) -> Principal {
     env.add_cycles(canister, 10_u128.pow(12)).await;
     env.install_canister(canister, wasm.to_vec(), args, None)
         .await;
-    canister
+    (env, canister)
+}
+
+/// Builds an upgrader canister client
+pub fn build_client(pocket: PocketIc, canister_principal: Principal, caller_principal: Principal) -> UpgraderCanisterClient<PocketIcClient> {
+    let client = PocketIcClient::from_client(pocket, canister_principal, caller_principal);
+    UpgraderCanisterClient::new(client)
 }
